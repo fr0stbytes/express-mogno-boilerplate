d9f2f0495c334bf5b9b43a1260d4960dc691f1d1
Maria Vaporidi <m.vaporidi@gmail.com>
2020-01-04 17:14:17 +0200

    connected to mongodb

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 
 ## **(WIP)** A server boilerplate using express, mongoose and jest
 
-Includes complete functionality for **User Authentication** (local auth with email & password / JWT) and CRUD operations for
+⏳ Will include complete functionality for **User Authentication** (local auth with email & password / JWT) and CRUD operations for
 **Posts** (with various fields/types) which can easily modified for any other data type.
 _____
 **// TODO:**
+1. ✔️ Connect to the database (atlasDB)
+2. [] Write test specs for Users
+3. [] Write test specs for Posts
+4. [] Implement User Authentication and Authorization
+5. [] Implement Post CRUD
 
-1. [] Write test specs for Users
-2. [] Write test specs for Posts
-3. [] Implement User Authentication and Authorization
-4. [] Implement Post CRUD
+## Setup
+
+1. **Connect to the Database (mongodb Atlas):**
+
+Create a **.env** file at the root of the app and add
+```process.env.DATABASE_URL = "your database path"```
+
+Make sure your ip is whitelisted inside Atlas or use ```0.0.0.0/0``` to whitelist all connections
+
+***Make sure to change this setting when in production**
--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Database Connection
+var mongoose = require('mongoose')
 
-var app = express();
+// Create a .env file at the root of the app and add process.env.DATABASE_URL = "your database path"
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() => {
+//         // connector.useDb('sample_airbnb?');
+//         const Listing = mongoose.model(
+//             'listingsAndReviews',
+//             new mongoose.Schema({
+//                 name: String,
+//                 summary: String,
+//             }),
+//             'sample_airbnb'
+//         );
+//         Listing.findOne({}).then((r) => {
+//             console.log(r);
+//         })
+//     })
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const db = mongoose.connection
+db.on('error', (error) => {
+  console.error(error)
+})
+db.once('open', () => {
+  console.log('connected to database')
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+var usersRouter = require('./routes/users')
 
-module.exports = app;
+var app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/users', usersRouter)
+
+module.exports = app
